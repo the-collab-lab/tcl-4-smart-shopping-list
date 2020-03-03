05f813b645d00ce7b91a41fd43a1ff913772a34c
Matthew J. Hughes <mjhughes707@gmail.com>
2020-03-02 18:15:22 -0800

    renamed parameter for consistency

--- src/estimates.js
+++ src/estimates.js
@@ -3,17 +3,17 @@
  * Current purchase a tiny bit less weight than all previous purchases
  * @param {Number} lastEstimate The last stored purchase interval estimate
  * @param {Number} latestInterval The interval between the most recent and previous purchases
- * @param {Number} numberOfPurchases Total number of purchases for the item
+ * @param {Number} numOfPurchases Total number of purchases for the item
  */
-const calculateEstimate = (lastEstimate, latestInterval, numberOfPurchases) => {
+const calculateEstimate = (lastEstimate, latestInterval, numOfPurchases) => {
   if (isNaN(lastEstimate)) {
     lastEstimate = 14;
   }
 
   // FIXME algorithm doesn't work when there's only 1 purchase in the database
-  let previousFactor = lastEstimate * numberOfPurchases;
-  let latestFactor = latestInterval * (numberOfPurchases - 1);
-  let totalDivisor = numberOfPurchases * 2 - 1;
+  let previousFactor = lastEstimate * numOfPurchases;
+  let latestFactor = latestInterval * (numOfPurchases - 1);
+  let totalDivisor = numOfPurchases * 2 - 1;
   return (previousFactor + latestFactor) / totalDivisor;
 };
 
